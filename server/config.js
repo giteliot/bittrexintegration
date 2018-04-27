be1371c0a604b518712c6e7d21eb1bd36b7ad90e
ELIOT <eliachinellato@gmail.com>
2018-04-27 11:59:41 +0200

    Restored Conifg

--- server/config.js
+++ server/config.js
@@ -13,11 +13,11 @@ config.VALIDSPIKE_MEM = 4;
 
 //Spikes Settings
 config.MAX_SPIKE = 10; //max spike size recordable; lower sizes are considered not valid
-config.MIN_CHANGE = 1; //mimum value of a spike to be considered a switch (5)
+config.MIN_CHANGE = 5; //mimum value of a spike to be considered a switch (5)
 
 //Analysis Settings
-config.MIN_SPIKETRADE = 0.2; //rebound size to consider a trade (1)
-config.ALERTRANK = 2; //minimum switches for a market to be tradable (4)
+config.MIN_SPIKETRADE = 1; //rebound size to consider a trade (1)
+config.ALERTRANK = 4; //minimum switches for a market to be tradable (4)
 
 //Buy Amount
 config.BUY_AM_BTC = 0.01;
